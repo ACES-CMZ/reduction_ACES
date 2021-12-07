675e70cbcd1258206aea36db2be65221674ffb7d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2021-12-07 12:17:38 -0500

    update mosaic script

--- imaging/mosaic_7m.py
+++ imaging/mosaic_7m.py
@@ -7,16 +7,16 @@ from reproject.mosaicking import find_optimal_celestial_wcs, reproject_and_coadd
 
 basepath = '/orange/adamginsburg/ACES/'
 
-target_wcs = wcs.WCS(naxis=2)
-target_wcs.wcs.ctype = ['GLON-CAR', 'GLAT-CAR']
-target_wcs.wcs.crval = [0, 0]
-target_wcs.wcs.cunit = ['deg', 'deg']
-target_wcs.wcs.cdelt = [-6.38888888888888e-4, 6.388888888888888e-4]
-target_wcs.wcs.crpix = [2000, 2000]
-
-header = target_wcs.to_header()
-header['NAXIS1'] = 4000
-header['NAXIS2'] = 4000
+# target_wcs = wcs.WCS(naxis=2)
+# target_wcs.wcs.ctype = ['GLON-CAR', 'GLAT-CAR']
+# target_wcs.wcs.crval = [0, 0]
+# target_wcs.wcs.cunit = ['deg', 'deg']
+# target_wcs.wcs.cdelt = [-6.38888888888888e-4, 6.388888888888888e-4]
+# target_wcs.wcs.crpix = [2000, 2000]
+# 
+# header = target_wcs.to_header()
+# header['NAXIS1'] = 4000
+# header['NAXIS2'] = 4000
 
 import glob
 filelist = glob.glob(f'{basepath}/rawdata/2021.1.00172.L/s*/g*/m*/product/*16_18_20_22*cont.I.tt0.pbcor.fits')
@@ -37,3 +37,15 @@ array, footprint = reproject_and_coadd(hdus,
                                        reproject_function=reproject_interp)
 
 fits.PrimaryHDU(data=array, header=target_wcs.to_header()).writeto(f'{basepath}/mosaics/7m_continuum_mosaic.fits', overwrite=True)
+
+import pylab as pl
+from astropy import visualization
+pl.matplotlib.use('agg')
+fig = pl.figure(figsize=(20,7))
+ax = fig.add_subplot(111, projection=target_wcs)
+im = ax.imshow(array, norm=visualization.simple_norm(array, stretch='asinh'))
+pl.colorbar(mappable=im)
+ax.coords[0].set_axislabel('Galactic Longitude')
+ax.coords[1].set_axislabel('Galactic Latitude')
+
+fig.savefig(f'{basepath}/mosaics/7m_continuum_mosaic.png', bbox_inches='tight')
