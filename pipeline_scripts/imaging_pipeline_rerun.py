e9d906a03f040debc25176be8ebb098442d35cad
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-03-23 09:51:53 -0400

    updated imaging script

--- pipeline_scripts/imaging_pipeline_rerun.py
+++ pipeline_scripts/imaging_pipeline_rerun.py
@@ -6,6 +6,7 @@ context.set_state('ProjectSummary', 'proposal_title', 'unknown')
 context.set_state('ProjectSummary', 'piname', 'unknown')
 
 import os
+import shutil
 import sys
 cwd = os.getcwd()
 mous = cwd.split("/")[-3].split(".")[1].split("_")
@@ -18,7 +19,9 @@ context.set_state('ProjectStructure', 'ousstatus_entity_id', ous_entity_id)
 context.set_state('ProjectStructure', 'recipe_name', 'hifa_calimage_renorm')
 
 import glob
-mses = glob.glob("*.ms")
+# don't re-run this on things that already inclue 'target'
+# (mstransform will try again and again...)
+mses = [x for x in glob.glob("*.ms") if 'target' not in x]
 if len(mses) == 0:
     raise ValueError("No MSes found")
 
@@ -26,17 +29,32 @@ print(f"Starting imaging pipeline rerun for mses {mses} and for mouse {mous} in 
 
 try:
     hifa_importdata(vis=mses, dbservice=False)
-    hif_mstransform(pipelinemode="automatic")
+
+    # if _target.ms exists, we don't need to transform
+    if not all(os.path.exists(x.replace(".ms", "_target.ms")) for x in mses):
+        hif_mstransform(pipelinemode="automatic")
+
+    # per Tafoya, flagtargets is probably not needed (and it was already done once)
     #hifa_flagtargets(pipelinemode="automatic")
-    #hifa_imageprecheck(pipelinemode="automatic")
-    hif_makeimlist(specmode='mfs')
+
+    hifa_imageprecheck(pipelinemode="automatic")
+
+    if os.path.exists("../../calibration/cont.dat") and not os.path.exists('cont.dat'):
+        shutil.copy("../../calibration/cont.dat", '.')
+
     hif_findcont(pipelinemode="automatic")
     hif_uvcontfit(pipelinemode="automatic", fitorder=0)
     hif_uvcontsub(pipelinemode="automatic")
+
+    hif_makeimlist(specmode='mfs')
     hif_makeimages(pipelinemode="automatic")
+
     hif_makeimlist(specmode='cont')
     hif_makeimages(pipelinemode="automatic")
+
     hif_makeimlist(specmode='cube')
     hif_makeimages(pipelinemode="automatic")
+
+    hifa_exportdata(pipelinemode="automatic")
 finally:
     h_save()
