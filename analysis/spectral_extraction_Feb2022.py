84cda816ccdb92559505ab7d9756797094f1a790
lcolzi <95923167+lcolzi@users.noreply.github.com>
2022-03-28 11:08:46 +0200

    Update spectral_extraction_Feb2022.py

--- analysis/spectral_extraction_Feb2022.py
+++ analysis/spectral_extraction_Feb2022.py
@@ -16,13 +16,13 @@ class getslice(object):
 product_dict = {'brick_h2o_core': '/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/science_goal.uid___A001_X1590_X30a8/group.uid___A001_X1590_X30a9/member.uid___A001_X15a0_X192/',
                'SgrB2_G0.69': "/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/science_goal.uid___A001_X1590_X30a8/group.uid___A001_X1590_X30a9/member.uid___A001_X15a0_Xea/",
                'cloud_e_hotcore': "/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/science_goal.uid___A001_X1590_X30a8/group.uid___A001_X1590_X30a9/member.uid___A001_X15a0_X1a4/",
-               'cloud_c_hotcore': "/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/science_goal.uid___A001_X1590_X30a8/group.uid___A001_X1590_X30a9/member.uid___A001_X15a0_X1a4/"
+               'SgrC_hotcore': "/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/science_goal.uid___A001_X1590_X30a8/group.uid___A001_X1590_X30a9/member.uid___A001/X15a0/X176/"
         }
 
 regions_dict = {'brick_h2o_core': regions.CircleSkyRegion(coordinates.SkyCoord('17:46:10.628', '-28:42:17.75', frame='icrs', unit=(u.h, u.deg)), radius=15*u.arcsec),
                 'SgrB2_G0.69': regions.CircleSkyRegion(coordinates.SkyCoord('17h47m22s', '-28:21:27', frame='fk5', unit=(u.h, u.deg)),  radius=15*u.arcsec),
                 'cloud_e_hotcore': regions.CircleSkyRegion(coordinates.SkyCoord(0.4751733*u.deg, -0.0096808*u.deg, frame='galactic'), radius=15*u.arcsec),
-                'cloud_c_hotcore': regions.CircleSkyRegion(coordinates.SkyCoord('17:46:21.42', '-28:35:40.40', frame='icrs', unit=(u.h, u.deg)), radius=15*u.arcsec),
+                'SgrC_hotcore': regions.CircleSkyRegion(coordinates.SkyCoord('17:44:40.58', '-29:28:16.28', frame='icrs', unit=(u.h, u.deg)), radius=15*u.arcsec),
                 }
 
     
