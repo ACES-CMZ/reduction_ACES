be1f08aa17767ac8ca4ffef12a644b3d58166889
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-04-05 11:20:28 -0400

    automation work

--- hipergator_scripts/delivery_status.py
+++ hipergator_scripts/delivery_status.py
@@ -11,17 +11,14 @@ from astropy.stats import mad_std
 from astropy import log
 import pylab as pl
 import subprocess
+from mous_map import get_mous_to_sb_mapping
 
-def get_mous_to_sb_mapping(project_code):
-    from astroquery.alma import Alma
+def get_mousmap_(**kwargs):
+    mousmap = get_mous_to_sb_mapping('2021.1.00172.L', **kwargs)
+    mousmap_ = {key.replace("/","_").replace(":","_"):val for key,val in mousmap.items()}
+    return mousmap_
 
-    tbl = Alma.query(payload={'project_code': project_code}, cache=False,
-                     public=False)['member_ous_uid','schedblock_name', 'qa2_passed']
-    mapping = {row['member_ous_uid']: row['schedblock_name'] for row in tbl if row['qa2_passed'] == 'T'}
-    return mapping
-
-mousmap = get_mous_to_sb_mapping('2021.1.00172.L')
-mousmap_ = {key.replace("/","_").replace(":","_"):val for key,val in mousmap.items()}
+mousmap_ = get_mousmap_()
 
 cwd = os.getcwd()
 basepath = '/orange/adamginsburg/ACES/rawdata/'
@@ -29,8 +26,8 @@ os.chdir(basepath)
 
 datatable = {}
 
-spwlist = {'12M': {33, 35, 25, 27, 29, 31},
-           'TM1': {33, 35, 25, 27, 29, 31},
+spwlist = {'12M': {25, 27, 29, 31, 33, 35, },
+           'TM1': {25, 27, 29, 31, 33, 35, },
            '7M': {16, 18, 20, 22, 24, 26},
            'TP':  {16, 18, 20, 22, 24, 26},
            }
@@ -41,9 +38,16 @@ def wildexists(x):
 basepath = dataroot = '/orange/adamginsburg/ACES/data/2021.1.00172.L'
 #workpath = '/blue/adamginsburg/adamginsburg/ACES/workdir/'
 
-for fullpath in glob.glob(f"{basepath}/sci*/group*/member*/"):
+looplist = glob.glob(f"{basepath}/sci*/group*/member*/")
+looplist = sorted(looplist, key=lambda x: os.path.basename(x))
+
+for fullpath in looplist:
     mous = os.path.basename(fullpath.strip('/')).split(".")[-1]
-    sbname = mousmap_[mous]
+    try:
+        sbname = mousmap_[mous]
+    except KeyError:
+        mousmap_ = get_mousmap_(refresh=True)
+        sbname = mousmap_[mous]
     field = sbname.split("_")[3]
     config = sbname.split("_")[5]
     if ' ' in config:
@@ -56,16 +60,16 @@ for fullpath in glob.glob(f"{basepath}/sci*/group*/member*/"):
             #globblob = f'{fullpath}/calibrated/working/*{clean}*.iter1{suffix}'
             #fn = glob.glob(f'{dataroot}/{globblob}')
 
-            for spwn in spwlist[config] | {'aggregate'} if clean=='mfs' else set():
+            for spwn in sorted(spwlist[config] | {'aggregate'} if clean=='mfs' else set(), key=lambda x: str(x)):
                 # /orange/adamginsburg/ACES/rawdata/2021.1.00172.L/
                 # science_goal.uid___A001_X1590_X30a8/group.uid___A001_X1590_X30a9/member.uid___A001_X15a0_Xae/calibrated/working/uid___A001_X15a0_Xae.s9_0.Sgr_A_star_sci.spw26.mfs.I.iter1.image
-                spw = spw if isinstance(spw, str) else f'spw{spw}'
+                spw = spwn if isinstance(spwn, str) else f'spw{spwn}'
                 bn = f'{mous}.s*_0.Sgr_A_star_sci.{spw}.{clean}.I'
                 workingpath = f'{fullpath}/calibrated/working/'
 
-                imageglob = f'{workingpath}/{bn}.image'
-                pbcorglob = f'{workingpath}/{bn}.image.pbcor'
-                psfglob = f'{workingpath}/{bn}.psf'
+                imageglob = f'{workingpath}/{bn}.iter1.image'
+                pbcorglob = f'{workingpath}/{bn}.iter1.image.pbcor'
+                psfglob = f'{workingpath}/{bn}.iter1.psf'
 
                 exists = (wildexists(pbcorglob)
                           or ("WIPim" if wildexists(imageglob)
@@ -96,6 +100,28 @@ for fullpath in glob.glob(f"{basepath}/sci*/group*/member*/"):
 import json
 with open('/orange/adamginsburg/web/secure/ACES/tables/imaging_completeness_grid.json', 'w') as fh:
     json.dump(datatable, fh)
+
+# make a table
+from astropy.table import Table
+tables = {}
+for field,xx in datatable.items():
+    cols = {}
+    tables[field] = cols
+    for config, yy in xx.items():
+        rows = []
+        for spw, zz in yy.items():
+            for imtype, ww in zz.items():
+                rows = ww
+                #cols[f'{config}.{spw}'].append(','.join([argtype for argtype, status in ww.items() if status is True]))
+            cols[f'{config}.{spw}'] = rows
+    rows = [[cn,] + list(v.values()) for cn,v in cols.items()]
+    tables[field] = Table(rows=rows, names=['config.spw'] + list(ww.keys()))
+
+with open('/orange/adamginsburg/web/secure/ACES/tables/imaging_completeness_grid.txt', 'w') as fh:
+    for key,tb in tables.items():
+        fh.write(f'Field {key}:\n')
+        fh.write("\n".join(tb.pformat()))
+        fh.write("\n\n")
 
 
 os.chdir(cwd)
---
+++ hipergator_scripts/mous_map.py
@@ -0,0 +1,21 @@
+import os
+from astroquery.alma import Alma
+import json
+
+basepath = '/orange/adamginsburg/ACES/rawdata/'
+
+def get_mous_to_sb_mapping(project_code, refresh=False, mousmapfile=f'{basepath}/mous_mapping.json'):
+    if refresh or not os.path.exists(mousmapfile):
+        print("Downloading MOUS map from ALMA archive")
+        tbl = Alma.query(payload={'project_code': project_code}, cache=False,
+                         public=False)['member_ous_uid','schedblock_name', 'qa2_passed']
+        mapping = {row['member_ous_uid']: row['schedblock_name'] for row in tbl if row['qa2_passed'] == 'T'}
+        mapping = {row['member_ous_uid']: row['schedblock_name'] for row in tbl}
+        with open(mousmapfile, 'w') as fh:
+            json.dump(mapping, fh)
+    else:
+        print(f"Recovering MOUS map from {mousmapfile}")
+        with open(mousmapfile, 'r') as fh:
+            mapping = json.load(fh)
+    return mapping
+
