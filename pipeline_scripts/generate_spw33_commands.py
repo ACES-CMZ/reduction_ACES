0a826c568f71d5ce6e342bd1974989014c47e1e0
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-06-25 08:46:37 -0400

    update override_tclean_commands with spw33 stuff....in theory

--- pipeline_scripts/generate_spw33_commands.py
+++ pipeline_scripts/generate_spw33_commands.py
@@ -11,17 +11,20 @@ else:
     sys.path.append(rootdir)
     sys.path.append(f'{rootdir}/pipeline_scripts')
 
+from merge_tclean_commands import commands
+
 with open(f"{rootdir}/pipeline_scripts/default_tclean_commands.json", "r") as fh:
     default_commands = json.load(fh)
 
 with open(f"{rootdir}/pipeline_scripts/override_tclean_commands.json", "r") as fh:
     override_commands = json.load(fh)
+ncmds = (len(override_commands))
 
-from merge_tclean_commands import commands
 
 for key in commands:
     if 'TM' in key:
         if 'spw33' not in commands[key]['tclean_cube_pars']:
+            print(f"Adding {key}")
             spw33pars = commands[key]['tclean_cube_pars']['spw35']
             spw33pars['nchan'] = 3836
             spw33pars['start'] = "97.6660537907GHz"
@@ -32,6 +35,7 @@ for key in commands:
             else:
                 override_commands[key] = {'tclean_cube_pars': spw33pars}
         elif commands[key]['tclean_cube_pars']['spw33']['nchan'] < 3800:
+            print(f"Modifying {key}")
             spw33pars = {}
             spw33pars['nchan'] = 3836
             spw33pars['start'] = "97.6660537907GHz"
@@ -41,6 +45,8 @@ for key in commands:
                 override_commands[key]['tclean_cube_pars'] = spw33pars
             else:
                 override_commands[key] = {'tclean_cube_pars': spw33pars}
+
+assert len(override_commands) >= ncmds
 
 with open(f"{rootdir}/pipeline_scripts/override_tclean_commands.json", "w") as fh:
     json.dump(override_commands, fh)
