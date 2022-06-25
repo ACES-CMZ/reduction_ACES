de87015164f670a3bda71efa69f108c3ddeea323
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-06-25 14:55:55 -0400

    change widths

--- pipeline_scripts/generate_spw33_commands.py
+++ pipeline_scripts/generate_spw33_commands.py
@@ -21,15 +21,19 @@ with open(f"{rootdir}/pipeline_scripts/override_tclean_commands.json", "r") as f
 ncmds = (len(override_commands))
 
 
+chwid = '488244Hz'
+nchan = 3836
+start = "97.6660537907GHz"
+
 for key in commands:
     if 'TM' in key:
         if 'spw33' not in commands[key]['tclean_cube_pars']:
             print(f"Adding {key}")
             spw33pars = commands[key]['tclean_cube_pars']['spw35']
             spw33pars['imagename'] = spw33pars['imagename'].replace('35', '33')
-            spw33pars['nchan'] = 3836
-            spw33pars['start'] = "97.6660537907GHz"
-            spw33pars['width'] = "0.4882381MHz"
+            spw33pars['nchan'] = nchan
+            spw33pars['start'] = start
+            spw33pars['width'] = chwid
             spw33pars['threshold'] = '0.01Jy'
             spw33pars['spw'] = [x.replace('35','33') for x in spw33pars['spw']]
             if key in override_commands:
@@ -39,9 +43,9 @@ for key in commands:
         elif commands[key]['tclean_cube_pars']['spw33']['nchan'] < 3800:
             print(f"Modifying {key}")
             spw33pars = {}
-            spw33pars['nchan'] = 3836
-            spw33pars['start'] = "97.6660537907GHz"
-            spw33pars['width'] = "0.4882381MHz"
+            spw33pars['nchan'] = nchan
+            spw33pars['start'] = start
+            spw33pars['width'] = chwid
             spw33pars['threshold'] = '0.01Jy'
             if key in override_commands:
                 override_commands[key]['tclean_cube_pars']['spw33'] = spw33pars
