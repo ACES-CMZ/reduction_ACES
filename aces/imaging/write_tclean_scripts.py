73b3593a401c587f87bd46014b97351f5fd32dc7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-08-29 10:57:45 -0400

    overwrite existing images etc. (they _shoudln't_ exist...)

--- aces/imaging/write_tclean_scripts.py
+++ aces/imaging/write_tclean_scripts.py
@@ -171,6 +171,10 @@ def main():
                              f"flist = glob.glob('{tempdir_name}/{os.path.basename(tcpars['imagename'])}.*')",
                              "for fn in flist:",
                              f"    logprint(f'Moving {{fn}} to {os.path.dirname(tcpars['imagename'])}')",
+                             f"    if os.path.exists(f'{os.path.dirname(tcpars['imagename'])}/{{fn}}'):",
+                             f"        logprint(f'Removing {os.path.dirname(tcpars['imagename'])}/{{fn}} because it exists')",
+                             f"        assert 'iter1' in f'{os.path.dirname(tcpars['imagename'])}/{{fn}}'",  # sanity check - don't remove important directories!
+                             f"        shutil.rmtree(f'{os.path.dirname(tcpars['imagename'])}/{{fn}}')",
                              f"    shutil.move(fn, '{os.path.dirname(tcpars['imagename'])}/')",
                              ] +
                             [f"shutil.rmtree('{tempdir_name}/{os.path.basename(x)}')" for x in tcpars['vis']]
