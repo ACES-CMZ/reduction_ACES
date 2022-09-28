bb1b4d3fcef3226e326f061f73f42eba242b0958
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-09-28 13:14:41 -0400

    tests are failing in practice, so CI is lying to me.

--- aces/tests/test_json.py
+++ aces/tests/test_json.py
@@ -1,4 +1,5 @@
 import json
+import pytest
 from aces import conf
 rootdir = conf.basepath
 
@@ -10,3 +11,11 @@ with open(f"{rootdir}/reduction_ACES/aces/pipeline_scripts/aggregate_high_tclean
 
 with open(f"{rootdir}/reduction_ACES/aces/pipeline_scripts/override_tclean_commands.json", "r") as fh:
     override_commands = json.load(fh)
+
+
+@pytest.mark_parametrize('jsonfile', ('default_tclean_commands.json', 
+                                    'aggregate_high_tclean_commands.json',
+                                    'override_tclean_commands.json'))
+def test_json(jsonfile):
+    with open(f"{rootdir}/reduction_ACES/aces/pipeline_scripts/{jsonfile}", "r") as fh:
+        override_commands = json.load(fh)
