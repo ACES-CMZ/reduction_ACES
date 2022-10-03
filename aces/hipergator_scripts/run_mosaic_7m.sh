f253397781236a198ef5ef45837de763588f7e07
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2022-10-03 11:03:25 -0400

    split out mosaicing scripts - they may be too expensive to re-run repeatedly

--- aces/hipergator_scripts/run_mosaic_7m.sh
+++ aces/hipergator_scripts/run_mosaic_7m.sh
@@ -22,25 +22,25 @@ cd /orange/adamginsburg/ACES/rawdata
 
 echo "test import"
 /orange/adamginsburg/miniconda3/envs/python39/bin/python -c "import zipfile" || exit 1
-echo "Retrieve data"
-/orange/adamginsburg/miniconda3/envs/python39/bin/aces_retrieve_data keflavich True True || exit 1
-echo "Retrieve weblogs"
-/orange/adamginsburg/miniconda3/envs/python39/bin/aces_retrieve_weblogs keflavich || exit 1
+#echo "Retrieve data"
+#/orange/adamginsburg/miniconda3/envs/python39/bin/aces_retrieve_data keflavich True True || exit 1
+#echo "Retrieve weblogs"
+#/orange/adamginsburg/miniconda3/envs/python39/bin/aces_retrieve_weblogs keflavich || exit 1
 
 
-export WEBLOG_DIR=/orange/adamginsburg/web/secure/ACES/weblogs/
-export WEBLOG_DIR='/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/weblogs'
-
-echo "Make links"
-/orange/adamginsburg/miniconda3/envs/python39/bin/aces_make_humanreadable_links || exit 1
-echo "Update github"
-/orange/adamginsburg/miniconda3/envs/python39/bin/aces_ghapi_update || exit 1
+#export WEBLOG_DIR=/orange/adamginsburg/web/secure/ACES/weblogs/
+#export WEBLOG_DIR='/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/weblogs'
+#
+#echo "Make links"
+#/orange/adamginsburg/miniconda3/envs/python39/bin/aces_make_humanreadable_links || exit 1
+#echo "Update github"
+#/orange/adamginsburg/miniconda3/envs/python39/bin/aces_ghapi_update || exit 1
 
 
 #echo "Make 7m mosaic"
 #/orange/adamginsburg/miniconda3/envs/python39/bin/aces_mosaic_7m || exit 1
-#echo "Make TP mosaic"
-#/orange/adamginsburg/miniconda3/envs/python39/bin/aces_mosaic_TP || exit 1
+echo "Make TP mosaic"
+/orange/adamginsburg/miniconda3/envs/python39/bin/aces_mosaic_TP || exit 1
 #echo "Make 12m mosaic"
 #/orange/adamginsburg/miniconda3/envs/python39/bin/aces_mosaic_12m || exit 1
 
