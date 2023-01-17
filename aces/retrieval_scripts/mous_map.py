4b681a13538540b6693a3810bea24872213ecebb
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-01-17 09:14:52 -0500

    bugfix: public=False no longer works b/c some of our data are public

--- aces/retrieval_scripts/mous_map.py
+++ aces/retrieval_scripts/mous_map.py
@@ -10,8 +10,8 @@ def get_mous_to_sb_mapping(project_code, refresh=False, mousmapfile=f'{datapath}
     if refresh or not os.path.exists(mousmapfile):
         print("Downloading MOUS map from ALMA archive")
         tbl = Alma.query(payload={'project_code': project_code},
-                         public=False)['member_ous_uid', 'schedblock_name', 'qa2_passed']
-        mapping = {row['member_ous_uid']: row['schedblock_name'] for row in tbl if row['qa2_passed'] == 'T'}
+                         public=None)['member_ous_uid', 'schedblock_name', 'qa2_passed']
+        # obsolete mapping = {row['member_ous_uid']: row['schedblock_name'] for row in tbl if row['qa2_passed'] == 'T'}
         mapping = {row['member_ous_uid']: row['schedblock_name'] for row in tbl}
         with open(mousmapfile, 'w') as fh:
             json.dump(mapping, fh)
