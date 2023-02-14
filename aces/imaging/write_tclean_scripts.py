11d47a63f138b19e763f4a27200bd9936ca8fc39
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-02-14 16:38:25 -0500

    if dropping _targets.ms -> .ms, set datacolumn='corrected'

--- aces/imaging/write_tclean_scripts.py
+++ aces/imaging/write_tclean_scripts.py
@@ -90,6 +90,7 @@ def main():
                     print(f"{sbname} {mous} {partype} {spwsel} {field} {config}: ", end=" ")
                     if not all(os.path.exists(x) for x in tcpars['vis']) and os.getenv('TRYDROPTARGET'):
                         tcpars['vis'] = [x.replace("_targets", "") for x in tcpars["vis"]]
+                        tcpars['datacolumn'] = 'corrected'
 
                     # Nov 10, 2022: try removing "_lines" from files:
                     if all(os.path.exists(x.replace("_targets_line", "")) for x in tcpars['vis']):
