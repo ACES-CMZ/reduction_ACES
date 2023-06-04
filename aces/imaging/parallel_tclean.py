ed6a6f9f0413d9d7bb661acf0c2c152fd0c5e19a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-06-03 20:28:37 -0400

    aaaaaugh

--- aces/imaging/parallel_tclean.py
+++ aces/imaging/parallel_tclean.py
@@ -43,15 +43,19 @@ def parallel_clean_slurm(nchan, imagename, spw, start=0, width=1, nchan_per=128,
     # In the current version, there is probably a race condition.
 
     rename_vis = textwrap.dedent(f"""
-            def rename_vis(x):
-                return os.path.join("{workdir}",
-                                    os.path.basename(x).replace('.ms',
-                                                                f'_spw{spw}.ms'))
+
+        def rename_vis(x):
+            return os.path.join("{workdir}",
+                                os.path.basename(x).replace('.ms',
+                                                            f'_spw{spw}.ms'))
+
                                  """)
     logprint = textwrap.dedent("""
+
         def logprint(x):
             print(x, flush=True)
             casalog.post(str(x), origin='parallel_tclean')
+
                                """)
 
     splitcmd = textwrap.dedent(
@@ -97,63 +101,59 @@ def parallel_clean_slurm(nchan, imagename, spw, start=0, width=1, nchan_per=128,
         tclean_kwargs = {tclean_kwargs}
         width = {width}
         nchan_per = {nchan_per}
+        splitspw = {spw}
 
-
-        {logprint}
-
-        {rename_vis}
-
-    """)
+        """)
+    script += logprint
+    script += rename_vis
+    splitscript = script + splitcmd
 
     if hasunit:
         script += textwrap.dedent(f"""
-        start = int(os.getenv('SLURM_ARRAY_TASK_ID')) * {nchan_per} * {width} + {start}
-        tclean_kwargs['start'] = f'{{start}}GHz'
-        tclean_kwargs['width'] = f'{{width}}GHz'
-        startchan = int(os.getenv('SLURM_ARRAY_TASK_ID')) * {nchan_per}
-        tclean_kwargs['imagename'] = os.path.basename(f"{imagename}.{{startchan:04d}}.{{nchan_per:03d}}")
-        #splitspw = f'{spw}:{{start-width}}GHz~{{start+width*(nchan_per+1)}}GHz'
-        splitspw = {spw}
-        """)
+            start = int(os.getenv('SLURM_ARRAY_TASK_ID')) * {nchan_per} * {width} + {start}
+            tclean_kwargs['start'] = f'{{start}}GHz'
+            tclean_kwargs['width'] = f'{{width}}GHz'
+            startchan = int(os.getenv('SLURM_ARRAY_TASK_ID')) * {nchan_per}
+            tclean_kwargs['imagename'] = os.path.basename(f"{imagename}.{{startchan:04d}}.{{nchan_per:03d}}")
+            #splitspw = f'{spw}:{{start-width}}GHz~{{start+width*(nchan_per+1)}}GHz'
+            """)
     else:
         script += textwrap.dedent(f"""
-        startchan = start = int(os.getenv('SLURM_ARRAY_TASK_ID')) * {nchan_per} * {width} + {start}
-        tclean_kwargs['start'] = start
-        tclean_kwargs['width'] = width
-        tclean_kwargs['imagename'] = os.path.basename(f"{imagename}.{{start:04d}}.{{nchan_per:03d}}")
-        splitspw = {spw}
-        """)
+            startchan = start = int(os.getenv('SLURM_ARRAY_TASK_ID')) * {nchan_per} * {width} + {start}
+            tclean_kwargs['start'] = start
+            tclean_kwargs['width'] = width
+            tclean_kwargs['imagename'] = os.path.basename(f"{imagename}.{{start:04d}}.{{nchan_per:03d}}")
+            """)
 
-    splitscript = script + splitcmd
 
     script += textwrap.dedent(f"""
 
-    # rename the vises whether or not we image so cleanup will work
-    tclean_kwargs['vis'] = [rename_vis(vis) for vis in tclean_kwargs['vis']]
+        # rename the vises whether or not we image so cleanup will work
+        tclean_kwargs['vis'] = [rename_vis(vis) for vis in tclean_kwargs['vis']]
 
-    for vis in tclean_kwargs['vis']:
-        msmd.open(vis)
-        # assume spw=0
-        nchan_max = msmd.nchan(0)
-        msmd.close()
+        for vis in tclean_kwargs['vis']:
+            msmd.open(vis)
+            # assume spw=0
+            nchan_max = msmd.nchan(0)
+            msmd.close()
 
-    if start >= nchan_max:
-        logprint(f"Maximum number of channels is {{nchan_max}} and start={{start}}.  Quitting.")
-        sys.exit(0)
+        if start >= nchan_max:
+            logprint(f"Maximum number of channels is {{nchan_max}} and start={{start}}.  Quitting.")
+            sys.exit(0)
 
-    if os.path.exists(tclean_kwargs['imagename'] + ".image"):
-        logprint("Already done with startchan={{startchan}}")
-    else:
-        logprint(f'tclean_kwargs: {{tclean_kwargs}}')
-        logprint(tclean_kwargs['vis'])
-        logprint(f"Cleaning with startchan={{startchan}}")
+        if os.path.exists(tclean_kwargs['imagename'] + ".image"):
+            logprint("Already done with startchan={{startchan}}")
+        else:
+            logprint(f'tclean_kwargs: {{tclean_kwargs}}')
+            logprint(tclean_kwargs['vis'])
+            logprint(f"Cleaning with startchan={{startchan}}")
 
-        tclean(**tclean_kwargs)
+            tclean(**tclean_kwargs)
 
-        if not os.path.exists(tclean_kwargs['imagename'] + ".image"):
-            raise ValueError(f"FAILURE: image {{tclean_kwargs['imagename']}}.image was not produced")
+            if not os.path.exists(tclean_kwargs['imagename'] + ".image"):
+                raise ValueError(f"FAILURE: image {{tclean_kwargs['imagename']}}.image was not produced")
 
-    """)
+        """)
 
 
     splitscriptname = os.path.join(workdir, f"{imagename}_parallel_split_script.py")
@@ -174,9 +174,9 @@ def parallel_clean_slurm(nchan, imagename, spw, start=0, width=1, nchan_per=128,
         fh.write(runsplitcmd)
 
     slurmsplitcmd = (f'/opt/slurm/bin/sbatch --ntasks={ntasks} '
-                     f'--mem-per-cpu={mem_per_cpu} --output={jobname}_%j_%A_%a.log'
+                     f'--mem-per-cpu={mem_per_cpu} --output={jobname}_%j_%A_%a.log '
                      f'--job-name={jobname} --account={account} '
-                     f'--qos={qos} --export=ALL --time={jobtime} {slurmsplitcmdsh}')
+                     f'--qos={qos} --export=ALL --time={jobtime} {slurmsplitcmdsh}\n')
 
 
     if dry:
@@ -207,10 +207,11 @@ def parallel_clean_slurm(nchan, imagename, spw, start=0, width=1, nchan_per=128,
         fh.write(runcmd)
 
     cmd = (f'/opt/slurm/bin/sbatch --ntasks={ntasks} '
-           f'--mem-per-cpu={mem_per_cpu} --output={jobname}_%j_%A_%a.log --job-name={jobname} --account={account} '
+           f'--mem-per-cpu={mem_per_cpu} --output={jobname}_%j_%A_%a.log '
+           f'--job-name={jobname} --account={account} '
            f'--array=0-{NARRAY} '
            f'--dependency=afterok:{scriptjobid} '
-           f'--qos={qos} --export=ALL --time={jobtime} {slurmcmd}')
+           f'--qos={qos} --export=ALL --time={jobtime} {slurmcmd}\n')
 
     if dry:
         print(cmd.split())
@@ -223,47 +224,50 @@ def parallel_clean_slurm(nchan, imagename, spw, start=0, width=1, nchan_per=128,
 
 
     mergescriptname = os.path.join(workdir, imagename+"_merge_script.py")
+
+    # note the forced dedenting here because of {logprint} and {rename_vis}
     mergescript = textwrap.dedent(
-        f"""
-        import glob, os, shutil
-        savedir = '{savedir}'
-        os.chdir('{workdir}')
-        for suffix in ("image", "pb", "psf", "model", "residual", "weight", "mask", "image.pbcor", "sumwt"):
-            outfile = os.path.basename(f'{imagename}.{{suffix}}')
-            infiles = sorted(glob.glob(os.path.basename(f'{imagename}.[0-9]*.{{suffix}}')))
-            print(outfile, infiles)
-            if suffix in ("image.pbcor", "sumwt"):
-                # these may not always exist
-                print(f"Found only {{len(infiles)}} files: {{infiles}}.  suffix={{suffix}}")
-            else:
-                assert len(infiles) > 0, f"Found only {{len(infiles)}} files: {{infiles}}.  suffix={{suffix}}"
+f"""
+import glob, os, shutil
+savedir = '{savedir}'
+os.chdir('{workdir}')
+for suffix in ("image", "pb", "psf", "model", "residual", "weight", "mask", "image.pbcor", "sumwt"):
+    outfile = os.path.basename(f'{imagename}.{{suffix}}')
+    infiles = sorted(glob.glob(os.path.basename(f'{imagename}.[0-9]*.{{suffix}}')))
+    print(outfile, infiles)
+    if suffix in ("image.pbcor", "sumwt"):
+        # these may not always exist
+        print(f"Found only {{len(infiles)}} files: {{infiles}}.  suffix={{suffix}}")
+    else:
+        assert len(infiles) > 0, f"Found only {{len(infiles)}} files: {{infiles}}.  suffix={{suffix}}"
 
-            ia.imageconcat(outfile=outfile,
-                           infiles=infiles,
-                           mode='m')
-            if savedir and os.path.exists(savedir):
-                print(f"Moving {{outfile}} to {{savedir}}")
-                full_outfile = os.path.join(savedir, outfile)
-                if os.path.exists(full_outfile):
-                    print("Outfile {{full_outfile}} already exists.  Check what's up.")
-                else:
-                    shutil.move(outfile, savedir)
-            else:
-                print(f"Savedir {{savedir}} does not exist")
+    ia.imageconcat(outfile=outfile,
+                   infiles=infiles,
+                   mode='m')
+    if savedir and os.path.exists(savedir):
+        print(f"Moving {{outfile}} to {{savedir}}")
+        full_outfile = os.path.join(savedir, outfile)
+        if os.path.exists(full_outfile):
+            print("Outfile {{full_outfile}} already exists.  Check what's up.")
+        else:
+            shutil.move(outfile, savedir)
+    else:
+        print(f"Savedir {{savedir}} does not exist")
 
 
-        # Cleanup stage
+# Cleanup stage
 
-        import os, shutil
-        os.chdir('{workdir}')
-        tclean_kwargs = {tclean_kwargs}
+import os, shutil
+os.chdir('{workdir}')
+tclean_kwargs = {tclean_kwargs}
 
-        {rename_vis}
+{logprint}
+{rename_vis}
 
-        for vis in tclean_kwargs['vis']:
-            logprint(f"Removing visibility {{vis}}")
-            shutil.rmtree(vis)
-        """)
+for vis in tclean_kwargs['vis']:
+    logprint(f"Removing visibility {{vis}}")
+    shutil.rmtree(vis)
+""")
 
     with open(mergescriptname, 'w') as fh:
         fh.write(mergescript)
