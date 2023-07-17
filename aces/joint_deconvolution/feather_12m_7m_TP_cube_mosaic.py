10667ac08ca16fb4a6b96a4f46b57974f0464097
d-l-walker <danielwalker274@gmail.com>
2023-07-17 10:14:52 +0100

    Fix paths, update exceptions, wrap long lines

--- aces/joint_deconvolution/feather_12m_7m_TP_cube_mosaic.py
+++ aces/joint_deconvolution/feather_12m_7m_TP_cube_mosaic.py
@@ -1,23 +1,16 @@
-import os 
+import os
 import glob
 import warnings
 import numpy as np
 import pandas as pd
 from pathlib import Path
 from astropy.io import fits
-from astropy.wcs import WCS, FITSFixedWarning
 from reproject import reproject_interp
 from reproject.mosaicking import find_optimal_celestial_wcs
-from astropy.utils.exceptions import AstropyWarning
-from casatasks import feather, exportfits, imtrans, imreframe, imhead
-
-warnings.simplefilter('ignore', FITSFixedWarning)
-warnings.simplefilter('ignore', AstropyWarning)
-warnings.simplefilter('ignore', RuntimeWarning)
-
+from casatools import imhead, exportfits, imtrans, feather, imreframe
 
 def check_files_exist(file_names):
-    return all(Path(file_name).exists() for file_name in file_names)
+    return all(file_name is not None and Path(file_name).exists() for file_name in file_names)
 
 
 # Function to create fake HDUs for use in reprojecting
@@ -72,11 +65,13 @@ def weighted_reproject_and_coadd(cube_files, weight_files):
     return hdu_reproject
 
 
-def get_file(filename, expected_num=1):
-    files = list(Path(ACES_DATA).glob(filename))
-    if len(files) != expected_num:
-        raise ValueError(f"Expected exactly one file matching '{filename}', but found {len(files)}.")
-    return str(files[0])
+def get_file(filename):
+    path = Path(filename)
+    files = list(path.parent.glob(path.name))
+    if not files:
+        print(f"No files matching '{filename}' were found.")
+        return None
+    return str(files[-1])
 
 
 def export_fits(imagename, fitsimage):
@@ -84,15 +79,16 @@ def export_fits(imagename, fitsimage):
         exportfits(imagename=imagename, fitsimage=fitsimage, dropdeg=True)
 
 
-ACES_ROOTDIR = os.getenv('ACES_ROOTDIR')
-ACES_DATA = os.getenv('ACES_DATA')
-ACES_WORKDIR = os.getenv('ACES_WORKDIR')
+# Set the relevant paths
+ACES_ROOTDIR = Path(os.getenv('ACES_ROOTDIR'))
+ACES_DATA = Path(os.getenv('ACES_DATA'))
+ACES_WORKDIR = Path(os.getenv('ACES_WORKDIR'))
 Path.cwd = ACES_WORKDIR
 
-sb_names = pd.read_csv(ACES_DATA / 'aces_SB_uids.csv')
-molecule = 'HNCO'
-generic_name = '.Sgr_A_star_sci.spw'
-prefix = 'uid___A001_'
+"""
+Select the molecule/SPW based on the dictionary below, and the code will do the rest.
+"""
+MOLECULE = 'HNCO'
 
 line_spws = {
     "HCOp": {
@@ -117,6 +113,10 @@ line_spws = {
     }
 }
 
+sb_names = pd.read_csv(ACES_ROOTDIR / 'aces/data/tables/aces_SB_uids.csv')
+generic_name = '.Sgr_A_star_sci.spw'
+prefix = 'member.uid___A001_'
+
 # Loop over all the SBs and create the 12m+7m+TP cubes for the given molecule
 for i in range(len(sb_names)):
     obs_id = sb_names['Obs ID'][i]
@@ -125,69 +125,104 @@ for i in range(len(sb_names)):
     tp_mous_id = sb_names['TP MOUS ID'][i]
     seven_m_mous_id = sb_names['7m MOUS ID'][i]
     twelve_m_mous_id = sb_names['12m MOUS ID'][i]
+    
+    seven_m_cube = get_file(
+        f"{ACES_DATA / (prefix + seven_m_mous_id) / 'calibrated/working'}/*{generic_name}{line_spws[MOLECULE]['mol_7m_spw']}.cube.I.iter1.image.pbcor"
+    )
+    tp_cube = get_file(
+        f"{ACES_DATA / (prefix + tp_mous_id) / 'product'}/*{generic_name}{line_spws[MOLECULE]['mol_TP_spw']}.cube.I.sd.fits"
+    )
+    twelve_m_cube = get_file(
+        f"{ACES_DATA / (prefix + twelve_m_mous_id) / 'calibrated/working'}/*{generic_name}{line_spws[MOLECULE]['mol_12m_spw']}.cube.I.iter1.image.pbcor"
+    )
+    twelve_m_wt = get_file( 
+        f"{ACES_DATA / (prefix + twelve_m_mous_id) / 'calibrated/working'}/*{generic_name}{line_spws[MOLECULE]['mol_12m_spw']}.cube.I.iter1.weight"
+    )
 
-    seven_m_cube = get_file(f'{prefix}{seven_m_mous_id}*{generic_name}{line_spws[molecule]["mol_7m_spw"]}.cube.I.iter1.image.pbcor')
-    seven_m_wt = get_file(f'{prefix}{seven_m_mous_id}*{generic_name}{line_spws[molecule]["mol_7m_spw"]}.cube.I.iter1.weight')
-    tp_cube = get_file(f'member.{prefix}{tp_mous_id}*{generic_name}{line_spws[molecule]["mol_TP_spw"]}.cube.I.sd.fits')
-    twelve_m_cube = get_file(f'{prefix}{twelve_m_mous_id}*{generic_name}{line_spws[molecule]["mol_12m_spw"]}.cube.I.iter1.image.pbcor')
-    twelve_m_wt = get_file(f'{prefix}{twelve_m_mous_id}*{generic_name}{line_spws[molecule]["mol_12m_spw"]}.cube.I.iter1.weight')
-
-    if check_files_exist([tp_cube, seven_m_cube, seven_m_wt, twelve_m_cube, twelve_m_wt]) and not os.path.isdir(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather.{molecule}.image'):
+    if check_files_exist([tp_cube, seven_m_cube, twelve_m_cube, twelve_m_wt]):
+        print(f'Processing {obs_dir}')
 
         tp_freq = imhead(tp_cube, mode='get', hdkey='restfreq')
         seven_m_freq = imhead(seven_m_cube, mode='get', hdkey='restfreq')
 
-        if tp_freq['value'] != seven_m_freq['value'] and not (Path(tp_cube).parent / tp_cube.replace('.fits', '.reframe')).is_dir():
-            imreframe(imagename=tp_cube, restfreq=seven_m_freq['value'] + ' Hz', output=tp_cube.replace('.fits', '.reframe'))
+        if tp_freq['value'] != seven_m_freq['value'] and not (
+                Path(tp_cube).parent / tp_cube.replace('.fits', '.reframe')
+        ).is_dir():
+            imreframe(
+                imagename=tp_cube,
+                restfreq=seven_m_freq['value'] + ' Hz',
+                output=tp_cube.replace('.fits', '.reframe')
+            )
 
         if not (Path(tp_cube).parent / tp_cube.replace('.fits', '.imtrans')).is_dir():
-            imtrans(imagename=tp_cube.replace('.fits', '.reframe') if (Path(tp_cube).parent / tp_cube.replace('.fits', '.reframe')).is_dir() else tp_cube, outfile=tp_cube.replace('.fits', '.imtrans'), order="0132")
+            imtrans(
+                imagename=tp_cube.replace('.fits', '.reframe')
+                if (Path(tp_cube).parent / tp_cube.replace('.fits', '.reframe')).is_dir() else tp_cube,
+                outfile=tp_cube.replace('.fits', '.imtrans'),
+                order="0132"
+            )
 
-        if not (obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather.{molecule}.image').is_dir():    
-            feather(imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather.{molecule}.image'), highres=seven_m_cube, lowres=tp_cube.replace('.fits', '.imtrans'))
+        if not (obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather_all.{MOLECULE}.image').is_dir():    
+            feather(
+                imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather_all.{MOLECULE}.image'),
+                highres=seven_m_cube,
+                lowres=tp_cube.replace('.fits', '.imtrans')
+            )
 
-        tp_7m_cube = str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather.{molecule}.image')
+        tp_7m_cube = str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather_all.{MOLECULE}.image')
         tp_7m_cube_freq = imhead(tp_7m_cube, mode='get', hdkey='restfreq')
         twelve_m_freq = imhead(twelve_m_cube, mode='get', hdkey='restfreq')
+
         if tp_7m_cube_freq['value'] != twelve_m_freq['value'] and not (Path(tp_7m_cube) / '.reframe').is_dir():
-            imreframe(imagename=tp_7m_cube, restfreq=twelve_m_freq['value'] + ' Hz', output=tp_7m_cube + '.reframe')
+            imreframe(
+                imagename=tp_7m_cube,
+                restfreq=twelve_m_freq['value'] + ' Hz',
+                output=tp_7m_cube + '.reframe'
+            )
 
-        if not (obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather.{molecule}.image').is_dir():
-            feather(imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather.{molecule}.image'), highres=twelve_m_cube, lowres=tp_7m_cube + '.reframe' if (Path(tp_7m_cube) / '.reframe').is_dir() else tp_7m_cube)
+        if not (obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather_all.{MOLECULE}.image').is_dir():
+            feather(
+                imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather_all.{MOLECULE}.image'),
+                highres=twelve_m_cube,
+                lowres=tp_7m_cube + '.reframe' if (Path(tp_7m_cube) / '.reframe').is_dir() else tp_7m_cube
+            )
 
-        export_fits(imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather.{molecule}.image'),
-                    fitsimage=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather.{molecule}.image.fits'))
+        export_fits(
+            imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather_all.{MOLECULE}.image'),
+            fitsimage=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather_all.{MOLECULE}.image.fits')
+        )
 
-        export_fits(imagename=seven_m_wt,
-                    fitsimage=str(obs_dir / f'Sgr_A_st_{obs_id}.7M.{molecule}.image.weight.fits'))
+        export_fits(
+            imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather_all.{MOLECULE}.image'),
+            fitsimage=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather_all.{MOLECULE}.image.fits')
+        )
 
-        export_fits(imagename=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather.{molecule}.image'),
-                        fitsimage=str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather.{molecule}.image.fits'))
-
-        export_fits(imagename=twelve_m_wt,
-                        fitsimage=str(obs_dir / f'Sgr_A_st_{obs_id}.12M.{molecule}.image.weight.fits'))
+        export_fits(
+            imagename=twelve_m_wt,
+            fitsimage=str(obs_dir / f'Sgr_A_st_{obs_id}.12M.{MOLECULE}.image.weight.fits')
+        )
 
         intermediary_files = [
             tp_cube.replace('.fits', '.reframe'),
             tp_cube.replace('.fits', '.imtrans'),
-            str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather.{molecule}.image'),
+            str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_feather_all.{MOLECULE}.image'),
             tp_7m_cube + '.reframe',
-            str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather.{molecule}.image')
+            str(obs_dir / f'Sgr_A_st_{obs_id}.TP_7M_12M_feather_all.{MOLECULE}.image')
         ]
 
         for file in intermediary_files:
             try:
                 os.remove(file)
             except FileNotFoundError:
-                print(f"File {file} not found. Skipping...")
+                print(f"File {file} not found. I cannot delete what does not exist.")
 
     else:
-        print(f"One or more files do not exist for observation Sgr_A_st_{obs_id}. Skipping this one ...")
+        print(f"One or more cubes do not exist for observation Sgr_A_st_{obs_id}. Skipping this one ...")
 
 # Create a weighted cube mosaic of the TP+7m+12m data
-TP_7M_12M_cube_files = [str(x) for x in ACES_WORKDIR.glob(f'**/*.TP_7M_12M_feather.{molecule}.image.fits')]
-TWELVE_M_weight_files = [str(x) for x in ACES_WORKDIR.glob(f'**/*.12M.{molecule}.image.weight.fits')]
+TP_7M_12M_cube_files = [str(x) for x in ACES_WORKDIR.glob(f'**/*.TP_7M_12M_feather_all.{MOLECULE}.image.fits')]
+TWELVE_M_weight_files = [str(x) for x in ACES_WORKDIR.glob(f'**/*.12M.{MOLECULE}.image.weight.fits')]
 
-if not (ACES_WORKDIR / f'{molecule}.TP_7M_12M_weighted_mosaic.fits').exists():
+if not (ACES_WORKDIR / f'{MOLECULE}.TP_7M_12M_weighted_mosaic.fits').exists():
     TP_7M_12M_mosaic_hdu = weighted_reproject_and_coadd(TP_7M_12M_cube_files, TWELVE_M_weight_files)
-    TP_7M_12M_mosaic_hdu.writeto(f'{molecule}.TP_7M_12M_weighted_mosaic.fits')
+    TP_7M_12M_mosaic_hdu.writeto(f'{MOLECULE}.TP_7M_12M_weighted_mosaic.fits')
