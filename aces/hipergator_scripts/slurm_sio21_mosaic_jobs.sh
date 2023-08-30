e598b1ece16dd9a0d50a32985f67bdbb9fe39971
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-08-30 14:43:36 -0400

    fix more .sh typos

--- aces/hipergator_scripts/slurm_sio21_mosaic_jobs.sh
+++ aces/hipergator_scripts/slurm_sio21_mosaic_jobs.sh
@@ -11,12 +11,12 @@ for ch in `seq 0 ${incr} 350`; do
         --account=astronomy-dept --qos=astronomy-dept-b \
         --ntasks=8 --nodes=1 --mem=32gb --time=96:00:00 --parsable \
         --wrap "/blue/adamginsburg/adamginsburg/miniconda3/envs/python39/bin/python -c \"from aces.imaging.mosaic_12m import make_giant_mosaic_cube_sio21; make_giant_mosaic_cube_sio21(channels=range(${ch1},${ch2}), skip_final_combination=True, verbose=True,)\"")
-    jobids+=$jobid+":"
+    jobids+="${jobid}:"
 done
 
 echo "Job IDs are ${jobids}"
 
-sbatch --job_name=aces_sio21_mosaic_merge \
+sbatch --job-name=aces_sio21_mosaic_merge \
     --output=/blue/adamginsburg/adamginsburg/ACES/logs/aces_sio21_mosaic_merge_%j.log  \
     --dependency=afterok:$jobids \
     --account=astronomy-dept --qos=astronomy-dept-b \
