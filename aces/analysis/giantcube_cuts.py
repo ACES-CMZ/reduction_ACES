d84fee555fa7dd3a31fc7377bcaf47692a3fde2b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-08-30 11:59:05 -0400

    add an analysis script

---
+++ aces/analysis/giantcube_cuts.py
@@ -0,0 +1,85 @@
+#from dask.diagnostics import ProgressBar
+#pbar = ProgressBar(minimum=20) # don't show pbar <20s
+#pbar.register()
+import time
+from spectral_cube import SpectralCube
+import os
+
+if __name__ == "__main__":
+    dodask = False
+
+    if os.getenv('MOLNAME'):
+        molname = os.getenv('MOLNAME')
+    else:
+        molname = 'CS21'
+
+    if not dodask:
+        print("Before imports.  Slice-wise reduction", flush=True)
+
+        t0 = time.time()
+
+        cube = SpectralCube.read(f'{molname}_CubeMosaic.fits')
+        print(cube)
+
+        print(f"PV peak intensity.  dt={time.time()-t0}", flush=True)
+        pv_max = cube.max(axis=1, how='slice')
+        pv_max.write(f"{molname}_CubeMosaic_PV_max.fits", overwrite=True)
+
+        print(f"PV mean.  dt={time.time()-t0}")
+        pv_mean = cube.mean(axis=1, how='slice')
+        pv_mean.write(f"{molname}_CubeMosaic_PV_mean.fits", overwrite=True)
+
+        print(f"mom0.  dt={time.time()-t0}")
+        mom0 = cube.moment0(axis=0, how='slice')
+        mom0.write(f"{molname}_CubeMosaic_mom0.fits", overwrite=True)
+
+        print(f"max.  dt={time.time()-t0}")
+        mx = cube.max(axis=0, how='slice')
+        mx.write(f"{molname}_CubeMosaic_max.fits", overwrite=True)
+
+        print(f"PV max 2.  dt={time.time()-t0}")
+        pv_max = cube.max(axis=2, how='slice')
+        pv_max.write(f"{molname}_CubeMosaic_PV_b_max.fits", overwrite=True)
+
+        print(f"PV mean 2.  dt={time.time()-t0}")
+        pv_mean = cube.mean(axis=2, how='slice')
+        pv_mean.write(f"{molname}_CubeMosaic_PV_b_mean.fits", overwrite=True)
+
+    else:
+        print("Before imports", flush=True)
+
+        import dask
+        #dask.config.set(scheduler='threads')
+        from dask.distributed import Client, LocalCluster
+        cluster = LocalCluster()
+        client = Client(cluster)
+        print(f"dashboard: {cluster.dashboard_link}", flush=True)
+
+        t0 = time.time()
+
+        cube = SpectralCube.read(f'{molname}_CubeMosaic.fits', use_dask=True)
+        print(cube)
+
+        print(f"PV peak intensity.  dt={time.time()-t0}", flush=True)
+        pv_max = cube.max(axis=1)
+        pv_max.write(f"{molname}_CubeMosaic_PV_max.fits", overwrite=True)
+
+        print(f"PV mean.  dt={time.time()-t0}")
+        pv_mean = cube.mean(axis=1)
+        pv_mean.write(f"{molname}_CubeMosaic_PV_mean.fits", overwrite=True)
+
+        print(f"mom0.  dt={time.time()-t0}")
+        mom0 = cube.moment0(axis=0)
+        mom0.write(f"{molname}_CubeMosaic_mom0.fits", overwrite=True)
+
+        print(f"max.  dt={time.time()-t0}")
+        mx = cube.max(axis=0)
+        mx.write(f"{molname}_CubeMosaic_max.fits", overwrite=True)
+
+        print(f"PV max 2.  dt={time.time()-t0}")
+        pv_max = cube.max(axis=2)
+        pv_max.write(f"{molname}_CubeMosaic_PV_b_max.fits", overwrite=True)
+
+        print(f"PV mean 2.  dt={time.time()-t0}")
+        pv_mean = cube.mean(axis=2)
+        pv_mean.write(f"{molname}_CubeMosaic_PV_b_mean.fits", overwrite=True)
