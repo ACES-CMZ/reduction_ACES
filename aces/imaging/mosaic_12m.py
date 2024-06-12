d67dee20839a98584f3601e2137822900805cf15
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2024-06-12 09:08:06 -0400

    add hc15n, so21

--- aces/imaging/mosaic_12m.py
+++ aces/imaging/mosaic_12m.py
@@ -909,3 +909,70 @@ def make_giant_mosaic_cube_hn13c(**kwargs):
         make_downsampled_cube(f'{basepath}/mosaics/cubes/HN13C_CubeMosaic.fits',
                               f'{basepath}/mosaics/cubes/HN13C_CubeMosaic_downsampled9.fits',
                               )
+
+
+def make_giant_mosaic_cube_so21(**kwargs):
+
+    filelist = sorted(glob.glob('/orange/adamginsburg/ACES/upload/Feather_12m_7m_TP/SPW25/cubes/Sgr_A_st_*.TP_7M_12M_feather_all.SPW_25.image.statcont.contsub.fits'))
+
+    print(f"Found {len(filelist)} SO 3-2-containing spw25 files")
+
+    check_files(filelist)
+
+    weightfilelist = [get_weightfile(fn, spw=25) for fn in filelist]
+    for fn in weightfilelist:
+        assert os.path.exists(fn)
+
+    restfreq = 86.09395e9
+    cdelt_kms = 0.84455895
+
+    make_giant_mosaic_cube(filelist,
+                           weightfilelist=weightfilelist,
+                           reference_frequency=restfreq,
+                           cdelt_kms=cdelt_kms,
+                           cubename='SO21',
+                           nchan=350,
+                           beam_threshold=3.1 * u.arcsec,
+                           channelmosaic_directory=f'{basepath}/mosaics/SO21_Channels/',
+                           # we prefer to fail_if_dropped; enabling this for debugging
+                           fail_if_cube_dropped=False,
+                           **kwargs,)
+
+    if not kwargs.get('skip_final_combination') and not kwargs.get('test'):
+        make_downsampled_cube(f'{basepath}/mosaics/cubes/SO21_CubeMosaic.fits',
+                              f'{basepath}/mosaics/cubes/SO21_CubeMosaic_downsampled9.fits',
+                              )
+
+
+def make_giant_mosaic_cube_hc15n(**kwargs):
+
+    filelist = sorted(glob.glob('/orange/adamginsburg/ACES/upload/Feather_12m_7m_TP/SPW25/cubes/Sgr_A_st_*.TP_7M_12M_feather_all.SPW_25.image.statcont.contsub.fits'))
+
+    print(f"Found {len(filelist)} HC15N-containing spw25 files")
+
+    check_files(filelist)
+
+    weightfilelist = [get_weightfile(fn, spw=25) for fn in filelist]
+    for fn in weightfilelist:
+        assert os.path.exists(fn)
+
+    restfreq = 86.05496e9
+    cdelt_kms = 0.84455895
+    make_giant_mosaic_cube(filelist,
+                           weightfilelist=weightfilelist,
+                           reference_frequency=restfreq,
+                           cdelt_kms=cdelt_kms,
+                           cubename='HC15N',
+                           nchan=350,
+                           beam_threshold=3.1 * u.arcsec,
+                           channelmosaic_directory=f'{basepath}/mosaics/HC15N_Channels/',
+                           # we prefer to fail_if_dropped; enabling this for debugging
+                           fail_if_cube_dropped=False,
+                           **kwargs,)
+
+    if not kwargs.get('skip_final_combination') and not kwargs.get('test'):
+        make_downsampled_cube(f'{basepath}/mosaics/cubes/HC15N_CubeMosaic.fits',
+                              f'{basepath}/mosaics/cubes/HC15N_CubeMosaic_downsampled9.fits',
+                              )
+
+
