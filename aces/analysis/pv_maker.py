59a41a62d43b448cd36f61cc46c8d1694efddae1
Savannah Gramze <savannahgramze@login7.ufhpc>
2024-08-13 13:33:48 -0400

    bug fixes

--- aces/analysis/pv_maker.py
+++ aces/analysis/pv_maker.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 from astropy.visualization import simple_norm
 import spectral_cube
 from spectral_cube import SpectralCube
+import regions
+from regions import Regions
 
 # Path to where to save the PV diagrams
 save_path = '/orange/adamginsburg/ACES/broadline_sources/EVFs/images/'
@@ -59,10 +61,11 @@ def make_pv_b(cube, b, mol):
 
     reg = regions.RectangleSkyRegion(center=SkyCoord((L_MIN+L_MAX)/2., b, frame='galactic'), width=1.5*u.deg, height=1*u.arcmin)
     subcube = cube.subcube_from_regions([reg])
+    subcube.allow_huge_operations = True
     pv_mean = subcube.mean(axis=1)
-    pv_mean.save(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_mean.fits')
+    pv_mean.write(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_mean.fits')
     pv_max = subcube.max(axis=1)
-    pv_max.save(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_max.fits')
+    pv_max.write(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_max.fits')
 
 def make_pv_l(cube, l, mol):
     """ 
@@ -80,10 +83,11 @@ def make_pv_l(cube, l, mol):
 
     reg = regions.RectangleSkyRegion(center=SkyCoord(l, (B_MIN+B_MAX)/2., frame='galactic'), width=1*u.arcmin, height=0.5*u.deg)
     subcube = cube.subcube_from_regions([reg])
+    subcube.allow_huge_operations = True
     pv_mean = subcube.mean(axis=2)
-    pv_mean.save(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_mean.fits')
+    pv_mean.write(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_mean.fits')
     pv_max = subcube.max(axis=2)
-    pv_max.save(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_max.fits')
+    pv_max.write(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_max.fits')
 
 def plot_pv(pv, mol, pos, longitude=True):
     """
@@ -140,14 +144,16 @@ def make_pv_mol(cube_fn):
     list_l = make_position_list(L_MIN, L_MAX)
 
     for b in list_b:
+        print('Making PV diagram at b =', b)
         make_pv_b(cube, b, mol)
 
     for l in list_l:
+        print('Making PV diagram at l =', l)
         make_pv_l(cube, l, mol)
 
 def main():
     basepath = '/orange/adamginsburg/ACES/mosaics/cubes/'
-    cube_fn_CS = f'{basepath}/CS_CubeMosaic.fits'
+    cube_fn_CS = f'{basepath}/CS21_CubeMosaic.fits'
     
     make_pv_mol(cube_fn_CS)
 
