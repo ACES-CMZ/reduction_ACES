55adbe892c4c4af7437496300182c788bab07696
Savannah Gramze <savannahgramze@login11.ufhpc>
2024-08-19 16:05:10 -0400

    added flake8 code style requirements

--- aces/analysis/pv_maker.py
+++ aces/analysis/pv_maker.py
@@ -19,15 +19,16 @@ from regions import Regions
 save_path = '/orange/adamginsburg/ACES/broadline_sources/EVFs/images/'
 
 # Latitude extrema
-B_MIN = -0.27*u.deg
-B_MAX = 0.22*u.deg
+B_MIN = -0.27 * u.deg
+B_MAX = 0.22 * u.deg
 
 # Longitude extrema
-L_MIN = -0.59*u.deg
-L_MAX = 0.88*u.deg
+L_MIN = -0.59 * u.deg
+L_MAX = 0.88 * u.deg
 
-def make_position_list(amin, amax, step=0.5*u.arcmin, unit=u.arcmin):
-    """ 
+
+def make_position_list(amin, amax, step=0.5 * u.arcmin, unit=u.arcmin):
+    """
     Make a list of Sky positions from amin to amax with a given step size and unit.
     Ideally used to make a list of latitudes or longitudes for the centers of region cutouts.
 
@@ -43,51 +44,54 @@ def make_position_list(amin, amax, step=0.5*u.arcmin, unit=u.arcmin):
         Unit of the values.
     """
 
-    return (np.arange(amin.to(unit).value, amax.to(unit).value+1, step.value)*unit).to(u.deg)
+    return (np.arange(amin.to(unit).value, amax.to(unit).value + 1, step.value) * unit).to(u.deg)
 
-def make_pv_b(cube, b, mol):
-    """ 
+
+def make_pv_b(cube, latitude, mol):
+    """
     Make a PV diagram along a given latitude.
 
     Parameters
     ----------
     cube : SpectralCube
         SpectralCube object of the data.
-    b : Quantity
+    latitude : Quantity
         Latitude of the center of the region.
     mol : str
         Molecule name.
     """
 
-    reg = regions.RectangleSkyRegion(center=SkyCoord((L_MIN+L_MAX)/2., b, frame='galactic'), width=1.5*u.deg, height=1*u.arcmin)
+    reg = regions.RectangleSkyRegion(center=SkyCoord((L_MIN + L_MAX) / 2., latitude, frame='galactic'), width=1.5 * u.deg, height=1 * u.arcmin)
     subcube = cube.subcube_from_regions([reg])
     subcube.allow_huge_operations = True
     pv_mean = subcube.mean(axis=1)
-    pv_mean.write(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_mean.fits')
+    pv_mean.write(f'{save_path}/{mol}_pv_b{round(latitude.value, 3)}_mean.fits')
     pv_max = subcube.max(axis=1)
-    pv_max.write(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_max.fits')
+    pv_max.write(f'{save_path}/{mol}_pv_b{round(latitude.value, 3)}_max.fits')
 
-def make_pv_l(cube, l, mol):
-    """ 
+
+def make_pv_l(cube, longitude, mol):
+    """
     Make a PV diagram along a given longitude.
 
     Parameters
     ----------
     cube : SpectralCube
         SpectralCube object of the data.
-    l : Quantity
+    longitude : Quantity
         Longitude of the center of the region.
     mol : str
         Molecule name.
     """
 
-    reg = regions.RectangleSkyRegion(center=SkyCoord(l, (B_MIN+B_MAX)/2., frame='galactic'), width=1*u.arcmin, height=0.5*u.deg)
+    reg = regions.RectangleSkyRegion(center=SkyCoord(longitude, (B_MIN + B_MAX) / 2., frame='galactic'), width=1 * u.arcmin, height=0.5 * u.deg)
     subcube = cube.subcube_from_regions([reg])
     subcube.allow_huge_operations = True
     pv_mean = subcube.mean(axis=2)
-    pv_mean.write(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_mean.fits')
+    pv_mean.write(f'{save_path}/{mol}_pv_l{round(longitude.value, 3)}_mean.fits')
     pv_max = subcube.max(axis=2)
-    pv_max.write(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_max.fits')
+    pv_max.write(f'{save_path}/{mol}_pv_l{round(longitude.value, 3)}_max.fits')
+
 
 def plot_pv(pv, mol, pos, longitude=True, mean='mean', close=True):
     """
@@ -112,7 +116,7 @@ def plot_pv(pv, mol, pos, longitude=True, mean='mean', close=True):
     ax.imshow(pv.data, aspect='auto', vmin=vmin, vmax=vmax, cmap='inferno')
 
     ax11 = ax.coords[1]
-    ax11.set_format_unit(u.km/u.s)
+    ax11.set_format_unit(u.km / u.s)
     ax.set_ylabel('Velocity (km/s)')
 
     if longitude:
@@ -125,8 +129,9 @@ def plot_pv(pv, mol, pos, longitude=True, mean='mean', close=True):
         ax.set_title(f'{mol} {mean} PV diagram at b={pos.value}')
         plt.tight_layout()
         plt.savefig(f'{save_path}/{mol}_{mean}_pv_b{pos.value}.png')
-    if close: 
+    if close:
         plt.close()
+
 
 def make_pv_mol(cube_fn):
     """
@@ -144,18 +149,19 @@ def make_pv_mol(cube_fn):
     list_b = make_position_list(B_MIN, B_MAX)
     list_l = make_position_list(L_MIN, L_MAX)
 
-    for b in list_b:
-        print('Making PV diagram at b =', b)
-        make_pv_b(cube, b, mol)
+    for latitude in list_b:
+        print('Making PV diagram at b =', latitude)
+        make_pv_b(cube, latitude, mol)
 
-    for l in list_l:
-        print('Making PV diagram at l =', l)
-        make_pv_l(cube, l, mol)
+    for longitude in list_l:
+        print('Making PV diagram at l =', longitude)
+        make_pv_l(cube, longitude, mol)
+
 
 def main():
     basepath = '/orange/adamginsburg/ACES/mosaics/cubes/'
     cube_fn_CS = f'{basepath}/CS21_CubeMosaic.fits'
-    
+
     make_pv_mol(cube_fn_CS)
 
 
