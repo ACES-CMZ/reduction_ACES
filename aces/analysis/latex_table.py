76354dd0955e9c31f3d862e8e2f8ac096c5154ee
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2024-11-30 12:12:00 -0500

    next tables

--- aces/analysis/latex_table.py
+++ aces/analysis/latex_table.py
@@ -28,7 +28,7 @@ def make_latex_table(savename='continuum_data_summary'):
     good = (((np.char.find(tbl['filename'], 'preQA3') == -1) |
             (np.char.find(tbl['filename'], 'X15a0_X178') == -1) |
              (np.char.find(tbl['filename'], 'obsolete') == -1) #| False # TODO: check for more
-             ) & 
+             ) &
             (
              (np.char.find(tbl['spws'], 'v1') == -1) &
              ~(tbl['pbcor']) &
@@ -104,7 +104,7 @@ def make_latex_table(savename='continuum_data_summary'):
     formats = {key: lambda x: str(sigfig.round(str(x), sigfigs=2))
                for key in float_cols}
 
-    ftbl.write('continuum_data_summary_table.ecsv', format='ascii.ecsv', overwrite=True)
+    ftbl.write(f'{basepath}/tables/continuum_data_summary_table.ecsv', format='ascii.ecsv', overwrite=True)
 
     # caption needs to be *before* preamble.
     #latexdict['caption'] = 'Continuum Source IDs and photometry'
@@ -121,6 +121,100 @@ def make_latex_table(savename='continuum_data_summary'):
                overwrite=True, latexdict=latexdict)
 
     return ftbl
+
+
+def make_observation_table():
+    from astroquery.alma import Alma
+    from astropy.time import Time
+    import numpy as np
+    from astropy.table import Table
+
+    metadata = Alma.query(payload={'project_code': '2021.1.00172.L'})
+    metadata['t_min'] = Time(metadata['t_min'], format='mjd')
+    metadata['Observation Start Time'] = Time(metadata['t_min'], format='mjd')
+    metadata['Observation Start Time'].format = 'fits'
+
+    sub_meta = metadata['schedblock_name', 'Observation Start Time', 'pwv', 't_exptime', 's_resolution', 'spatial_scale_max']
+    usub_meta = Table(np.unique(sub_meta))
+
+    tm = np.char.find(usub_meta['schedblock_name'], '_TM') > 0
+    sm = np.char.find(usub_meta['schedblock_name'], '_7M') > 0
+    tp = np.char.find(usub_meta['schedblock_name'], '_TP') > 0
+
+    configuration_schedules = requests.get('https://almascience.eso.org/observing/observing-configuration-schedule/prior-cycle-observing-and-configuration-schedule')
+
+    from astropy.time import Time
+    from astropy.io import ascii
+
+    def try_read_table(index):
+        try:
+            return ascii.read(configuration_schedules.text, format='html', htmldict={'table_id': index})
+        except Exception as ex:
+            #print(ex)
+            return
+
+    config_sched_tbls = {ii:
+        try_read_table(ii)
+        for ii in range(1, 10)
+        if try_read_table(ii) is not None
+    }
+    assert len(config_sched_tbls) > 0
+
+    for ii, config_sched_tbl in config_sched_tbls.items():
+        config_sched_tbl['Start date'] = Time(config_sched_tbl['Start date'])
+        config_sched_tbl['End date'] = Time(config_sched_tbl['End date'])
+
+    usub_meta['cycle_id'] = np.zeros(len(usub_meta), dtype='int')
+    usub_meta['config_id'] = ['x'*11 for x in range(len(usub_meta))]
+    usub_meta['block'] = ['xx' for x in range(len(usub_meta))]
+    for row in usub_meta:
+        for cycle_id, config_sched_tbl in config_sched_tbls.items():
+            match = ((config_sched_tbl['Start date'] < row['Observation Start Time']) &
+                     (config_sched_tbl['End date'] >= row['Observation Start Time']))
+            if match.sum() > 0:
+                row['cycle_id'] = cycle_id
+                row['config_id'] = config_sched_tbl[match]['Approx Config.'][0]
+                row['block'] = config_sched_tbl[match]['Block'][0]
+                #print(f"Matched {row['schedblock_name']} to cycle {cycle_id} config {row['config_id']} block {row['block']}")
+
+    usub_meta['Field'] = [x.split("_")[3] for x in usub_meta['schedblock_name']]
+    usub_meta.rename_column('pwv', 'PWV')
+    usub_meta.rename_column('t_exptime', 'Exposure Time')
+    usub_meta.rename_column('s_resolution', 'Resolution')
+    usub_meta.rename_column('spatial_scale_max', 'LAS')
+    usub_meta.rename_column('config_id', 'Configuration')
+
+    colnames = ['Field', 'Observation Start Time', 'Configuration', 'PWV', 'Exposure Time', 'Resolution', 'LAS']
+    usub_meta['LAS'].unit = u.arcsec
+    usub_meta['Resolution'].unit = u.arcsec
+    usub_meta['Exposure Time'].unit = u.s
+    usub_meta['PWV'].unit = u.mm
+    usub_meta[colnames][tm].write(f'{basepath}/tables/observation_metadata_12m.ecsv', format='ascii.ecsv', overwrite=True)
+    usub_meta[colnames][sm].write(f'{basepath}/tables/observation_metadata_7m.ecsv', format='ascii.ecsv', overwrite=True)
+    usub_meta[colnames][tp].write(f'{basepath}/tables/observation_metadata_TP.ecsv', format='ascii.ecsv', overwrite=True)
+
+    #latexdict['header_start'] = '\\label{tab:observation_metadata_12m}'#\n\\footnotesize'
+    #latexdict['preamble'] = '\\caption{12m Observation Metadata}\n\\resizebox{\\textwidth}{!}{'
+    latexdict['col_align'] = 'l' * len(usub_meta.columns)
+    latexdict['tabletype'] = 'table*'
+    latexdict['tablefoot'] = ("}\\par\n"
+                             )
+
+    float_cols = ['PWV', 'Exposure Time', 'Resolution', 'LAS']
+
+    formats = {key: lambda x: strip_trailing_zeros('{0:0.3f}'.format(round_to_n(x, 2)), nsf=2)
+               for key in float_cols}
+    formats = {key: lambda x: str(sigfig.round(str(x), sigfigs=2))
+               for key in float_cols}
+
+    for sel, nm in ((tm, '12m'), (sm, '7m'), (tp, 'TP')):
+        latexdict['header_start'] = f'\\label{{tab:observation_metadata_{nm}}}'#\n\\footnotesize'
+        latexdict['preamble'] = f'\\caption{{{nm} Observation Metadata}}\n\\resizebox{{\\textwidth}}{{!}}{{'
+        usub_meta[colnames][sel].write(f"{basepath}/papers/continuum_data/observation_metadata_{nm}.tex",
+                                       formats=formats,
+                                       overwrite=True,
+                                       latexdict=latexdict)
+
 
 
 if __name__ == "__main__":
