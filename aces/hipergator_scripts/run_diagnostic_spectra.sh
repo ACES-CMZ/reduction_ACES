e4ac38407646692774fcb8e98cd97b7c0b3451e2
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2025-01-19 10:48:52 -0500

    add more hpg scripts

---
+++ aces/hipergator_scripts/run_diagnostic_spectra.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --mail-type=NONE          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=adamginsburg@ufl.edu     # Where to send mail
+#SBATCH --ntasks=16
+#SBATCH --nodes=1
+#SBATCH --mem=64gb                     # Job memory request
+#SBATCH --time=96:00:00               # Time limit hrs:min:sec
+#SBATCH --output=/blue/adamginsburg/adamginsburg/ACES/logs/ACES_diagnostic_spectra_%j.log
+#SBATCH --export=ALL
+#SBATCH --job-name=ACES_diagnostic_spectra
+#SBATCH --qos=adamginsburg-b
+#SBATCH --account=adamginsburg
+pwd; hostname; date
+
+export WORK_DIR="/blue/adamginsburg/adamginsburg/ACES/workdir"
+
+module load git
+
+which python
+which git
+
+git --version
+echo $?
+
+export IPYTHON=/orange/adamginsburg/miniconda3/envs/python310/bin/ipython
+
+cd ${WORK_DIR}
+echo ${WORK_DIR}
+
+export ACES_ROOTDIR="/orange/adamginsburg/ACES/reduction_ACES/"
+export SCRIPT_DIR="${ACES_ROOTDIR}/analysis"
+export PYTHONPATH=$SCRIPT_DIR
+
+echo $LOGFILENAME
+
+#export NO_PROGRESSBAR='True'
+#export ENVIRON='BATCH'
+
+# TODO: get this from ntasks?
+#export DASK_THREADS=8
+export DASK_THREADS=$SLURM_NTASKS
+
+env
+
+/orange/adamginsburg/miniconda3/envs/python310/bin/python -c "from aces.analysis import diagnostic_spectra; diagnostic_spectra.main()"
+/orange/adamginsburg/miniconda3/envs/python310/bin/python -c "from aces.analysis import continuum_selection_diagnostic_plots; continuum_selection_diagnostic_plots.assemble_new_contsels()"
---
+++ aces/hipergator_scripts/run_mosaic_12m_all_lines.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --mail-type=NONE          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=adamginsburg@ufl.edu     # Where to send mail
+#SBATCH --time=96:00:00               # Time limit hrs:min:sec
+#SBATCH --ntasks=32
+#SBATCH --mem=256gb
+#SBATCH --nodes=1 # exactly 1 node
+#SBATCH --time=96:00:00               # Time limit hrs:min:sec
+#SBATCH --qos=astronomy-dept-b
+#SBATCH --account=astronomy-dept
+#SBATCH --output=/blue/adamginsburg/adamginsburg/ACES/logs/ACES_mosaic12m_%j.log
+#SBATCH --job-name=ACES_mosaic12m
+#SBATCH --export=ALL
+#SBATCH --array=0-72
+
+
+date
+
+. ~/.gh_token
+echo $GITHUB_TOKEN
+
+cd /orange/adamginsburg/ACES/rawdata
+
+echo "test import"
+/orange/adamginsburg/miniconda3/envs/python310/bin/python -c "import zipfile" || exit 1
+#echo "Retrieve data"
+#/orange/adamginsburg/miniconda3/envs/python310/bin/aces_retrieve_data keflavich True True || exit 1
+#echo "Retrieve weblogs"
+#/orange/adamginsburg/miniconda3/envs/python310/bin/aces_retrieve_weblogs keflavich || exit 1
+
+
+#export WEBLOG_DIR=/orange/adamginsburg/web/secure/ACES/weblogs/
+#export WEBLOG_DIR='/orange/adamginsburg/ACES/rawdata/2021.1.00172.L/weblogs'
+#
+#echo "Make links"
+#/orange/adamginsburg/miniconda3/envs/python310/bin/aces_make_humanreadable_links || exit 1
+#echo "Update github"
+#/orange/adamginsburg/miniconda3/envs/python310/bin/aces_ghapi_update || exit 1
+
+
+#echo "Make 7m mosaic"
+#/orange/adamginsburg/miniconda3/envs/python310/bin/aces_mosaic_7m || exit 1
+#echo "Make TP mosaic"
+#/orange/adamginsburg/miniconda3/envs/python310/bin/aces_mosaic_TP || exit 1
+echo "Make 12m mosaic"
+/orange/adamginsburg/miniconda3/envs/python310/bin/aces_mosaic_12m --skip-cont --remake-cache || exit 1
+
+# technically shouldn't need to be re-run, but as I add new mosaics, it will
+# (but this causes a 'failed' error)
+#ln -s /orange/adamginsburg/ACES/mosaics/*png /orange/adamginsburg/web/secure/ACES/mosaics/
+
