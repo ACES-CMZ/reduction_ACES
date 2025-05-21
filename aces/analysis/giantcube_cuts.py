30f98000e673b9336bd0e9766190937ef30e6a4c
Xing Lu <xinglv.nju@gmail.com>
2025-05-21 13:22:13 +0800

    Update giantcube_cuts.py
    Change the Vlsr ranges of the masks for SO21 and H13CO+.

--- aces/analysis/giantcube_cuts.py
+++ aces/analysis/giantcube_cuts.py
@@ -292,10 +292,12 @@ def do_all_stats(cube, molname, mompath=f'{basepath}/mosaics/cubes/moments/',
 
     print(f"Howargs: {howargs}.  Using vmask.")
 
-    if molname in ('H13CO+', 'HC15N'):
+    if molname in ('HC15N'):
         vmask = velocity_mask(cube, vmin_pos=0 * u.km/u.s, vmin_neg=-100 * u.km/u.s)
+    elif molname in ('H13CO+', ):
+        vmask = velocity_mask(cube, vmin_pos=-50 * u.km/u.s, vmin_neg=-150 * u.km/u.s)
     elif molname in ('SO21', ):
-        vmask = velocity_mask(cube, vmax_pos=100 * u.km/u.s, vmax_neg=0 * u.km/u.s)
+        vmask = velocity_mask(cube, vmax_pos=150 * u.km/u.s, vmax_neg=50 * u.km/u.s)
     elif molname in ('CH3CHO',):
         vmask = velocity_mask(cube, vmax_pos=300 * u.km/u.s, vmax_neg=200 * u.km/u.s)
     elif molname in ('CS21', 'CS'):
