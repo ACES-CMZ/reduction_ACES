c3ad6499d1b3cb529ddf13c8764c5d7a01bac1fd
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2025-07-11 15:42:08 -0400

    fix labels

--- aces/analysis/correlation_plots.py
+++ aces/analysis/correlation_plots.py
@@ -32,8 +32,8 @@ def main():
     print(sgra_or_sgrb2.shape, flush=True)
 
 
-    for mol1, mol2 in itertools.combinations([ "H40a",  "SO21", "SO32",], 2):
-    #for mol1, mol2 in itertools.combinations(['continuum', 'continuum_feathered', "CH3CHO", "CS21", "H13CN", "H13COp", "H40a", "HC15N", "HC3N", "HCOP", "HCOP_mopra", "HCOP_noTP", "HN13C", "HNCO_7m12mTP", "NSplus", "SiO21", "SO21", "SO32",], 2):
+    #for mol1, mol2 in itertools.combinations([ "H40a",  "SO21", "SO32",], 2):
+    for mol1, mol2 in itertools.combinations(['continuum', 'continuum_feathered', "CH3CHO", "CS21", "H13CN", "H13COp", "H40a", "HC15N", "HC3N", "HCOP", "HCOP_mopra", "HCOP_noTP", "HN13C", "HNCO_7m12mTP", "NSplus", "SiO21", "SO21", "SO32",], 2):
         print(f'{mol1} vs {mol2}.   time={time.time() - t0:0.2f}', flush=True)
         fn1 = f'{basepath}/mosaics/cubes/moments/{mol1}_CubeMosaic_masked_hlsig_dilated_mom0.fits'
         fn2 = f'{basepath}/mosaics/cubes/moments/{mol2}_CubeMosaic_masked_hlsig_dilated_mom0.fits'
@@ -58,7 +58,7 @@ def main():
         fig, ax = pl.subplots(1, 1)
         ax.plot([xmin, xmax], [xmin, xmax], color='b', linestyle='--')
         ax.scatter(data1, data2, s=1, alpha=0.5, color='k', label=f'r={corr:.2f}')
-        ax.scatter(data1[sgra_or_sgrb2], data2[sgra_or_sgrb2], s=1, alpha=0.5, color='r', label=f'r={corr:.2f}')
+        ax.scatter(data1[sgra_or_sgrb2], data2[sgra_or_sgrb2], s=1, alpha=0.5, color='r', label=f'Sgr B2 & Sgr A*')
         ax.set_xlabel(f'{mol1} [K km s$^{{-1}}$]')
         ax.set_ylabel(f'{mol2} [K km s$^{{-1}}$]')
         if mol1 in contnames:
@@ -71,7 +71,7 @@ def main():
 
         pl.close('all')
 
-        bothpos = (data1 > 0) & (data2 > 0)
+        bothpos = (data1 > 0) & (data2 > 0) & ~sgra_or_sgrb2
         corr = np.corrcoef(data1[bothpos], data2[bothpos])[0, 1]
 
         fig, ax = pl.subplots(1, 1)
@@ -84,7 +84,7 @@ def main():
         #                                               bins=[np.geomspace(x5, x95, 25), np.geomspace(y5, y95, 25)],
         #                                               threshold=30, marker=',')
 
-        ax.scatter(data1[sgra_or_sgrb2 & bothpos], data2[sgra_or_sgrb2 & bothpos], s=1, alpha=0.5, color='r', label=f'r={corr:.2f}')
+        ax.scatter(data1[sgra_or_sgrb2 & bothpos], data2[sgra_or_sgrb2 & bothpos], s=1, alpha=0.5, color='r', label=f'Sgr B2 & Sgr A*')
 
         ax.set_xlabel(f'{mol1} [K km s$^{{-1}}$]')
         ax.set_ylabel(f'{mol2} [K km s$^{{-1}}$]')
